43108a94555a66943c2abb1a2d2ad8b51fe917a7
radluk <radluk@oculusindex.org>
2025-01-17 11:57:07 +0100

    correzione bozza

--- esList/esEsame/esamever.c
+++ esList/esEsame/esamever.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int verifica(int);
 
 int main(int argc, char * argv[]){
@@ -7,32 +10,31 @@ int main(int argc, char * argv[]){
 
 	pass = atoi(argv[1]);
 	ris = verifica(pass);
-	printf("result: %d", ris);
+	printf("result: %d\n", ris);
 
 	return 0;
 }
 
-int verifca(int num){
+int verifica(int num){
 
 	int i, cont, ris;
 	char str[7];
-	rsi = 1;
-
+	ris = 1;
 	if(num < 100000 || num > 999999)
 		ris = 0;
 
-	itoa(num, str, 10);
+  sprintf(str, "%d", num);
 	for(i = 0, cont = 0; i < 6; i++){
 		if(str[i] > str[i+1]){
 			ris = 0;
 		}
-		if(str[i] = str[i+1])
+		if(str[i] == str[i+1])
 			cont = 1;
 	}
 
 	if(!cont)
 		ris = 0;
 
-	return 1;
+	return ris;
 }
 		
